4ced0bd693dd3cf7fba0b060605ad51bd5f59b34
Eduardo Saito <easaitodev@gmail.com>
2024-10-04 15:15:26 -0300

    fix: change to next.config file

--- src/app/components/Taskbar.tsx
+++ src/app/components/Taskbar.tsx
@@ -11,7 +11,7 @@ const Taskbar = () => {
   return (
     <div className="fixed bottom-0 left-0 w-full bg-win95-gray h-10 flex justify-between items-center outline outline-1 outline-white border-t-1 border-solid border-win95-dark-gray text-sm font-win95">
       <div className="flex items-center space-x-2 ml-2">
-        <CustomButton name='Start Menu' icon='/images/icons/ico/windows.ico' text='Iniciar' isActive={showStartMenu} setActive={setShowStartMenu} />
+        <CustomButton name="Start Menu" icon="/portfolio/images/icons/ico/windows.ico" text="Iniciar" isActive={showStartMenu} setActive={setShowStartMenu} />
       </div>
       {showStartMenu && <StartMenu />}
       <div className="mr-4 font-win95">{currentTime}</div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ import DesktopIcon from './components/DesktopIcon';
 
 const Home = () => {
   return (
-    <div className="w-full h-screen bg-win95-light-gray relative bg-[url('/images/wallpaper.jpeg')] bg-cover bg-center">
+    <div className="w-full h-screen bg-win95-light-gray relative bg-[url('/portfolio/images/wallpaper.jpeg')] bg-cover bg-center">
       <div className="p-4 grid grid-flow-row auto-rows-max gap-4 w-36 h-full">
-        <DesktopIcon name="Meu Computador" icon="/images/icons/ico/computer_explorer.ico" />
-        <DesktopIcon name="Lixeira" icon="/images/icons/ico/recycle_bin_empty.ico" />
+        <DesktopIcon name="Meu Computador" icon="/portfolio/images/icons/ico/computer_explorer.ico" />
+        <DesktopIcon name="Lixeira" icon="/portfolio/images/icons/ico/recycle_bin_empty.ico" />
         {/* Outros ícones */}
       </div>
       <Taskbar />
