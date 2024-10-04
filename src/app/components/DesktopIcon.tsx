b0f977c0f75256c0f3c5196fec4420d3b3f91449
Eduardo Saito <easaitodev@gmail.com>
2024-10-04 14:33:36 -0300

    fix: change desktop icon width and height

--- src/app/components/DesktopIcon.tsx
+++ src/app/components/DesktopIcon.tsx
@@ -14,7 +14,7 @@ const DesktopIcon: React.FC<DesktopIconProps> = ({ name, icon }) => {
   return (
     <div className={`text-center hover:cursor-pointer
       ${isClicked ? '' : ''}`} onClick={() => setIsClicked(!isClicked)}>
-      <Image src={icon} alt={name} className="mx-auto h-8" />
+      <Image src={icon} alt={name} className="mx-auto" width="30" height="30"/>
       <p className="font-win95 text-xs bg-win95-blue text-white inline-block px-0.5">{name}</p>
     </div>
   );
