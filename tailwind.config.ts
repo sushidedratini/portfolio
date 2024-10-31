bf25ee84322cf0b88b48d5a2ed14351c4bf7d702
Eduardo Saito <easaitodev@gmail.com>
2024-10-30 22:28:20 -0300

    feat: add MS Sans Serif font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,13 +9,15 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        'win95-blue': '#008080',
-        'win95-gray': '#C0C0C0',
-        'win95-dark-gray': '#808080',
-        'win95-light-gray': '#F0F0F0',
+        'win98-blue': '#0000FF',
+        'win98-teal-blue': '#008080',
+        'win98-dark-blue': '#000080',
+        'win98-gray': '#C0C0C0',
+        'win98-dark-gray': '#808080',
+        'win98-light-gray': '#F0F0F0',
       },
       fontFamily: {
-        win95: ['"MS Sans Serif"', 'sans-serif'], // Fonte característica do Windows 95
+        win98: ['var(--font-ms-sans-serif)'], 
       },
     },
   },
