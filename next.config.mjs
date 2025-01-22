693c68fec911136ca488e03ca40da00af044f35b
Eduardo Saito <easaitodev@gmail.com>
2025-01-22 13:25:47 -0300

    build: add pdf library and lint

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,11 @@ const nextConfig = {
     images: {
         unoptimized: true,
     },
+    // webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    //     config.resolve.alias.canvas = false
+    //     config.resolve.alias.encoding = false
+    //     return config
+    // }
 };
 
 export default nextConfig;
